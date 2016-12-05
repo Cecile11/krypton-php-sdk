debf5a261aff0ea7c2743ad9d475435c51faf8cb
Lionel Martin <lm@purebilling.com>
2016-12-05 19:10:44 +0100

    Add autoload file, if you don't want to use composer

---
+++ src/autoload.php
@@ -0,0 +1,5 @@
+<?php
+
+require(dirname(__FILE__) . '/src/Payzen/Constants.php');
+require(dirname(__FILE__) . '/src/Payzen/Exceptions/PayzenException.php');
+require(dirname(__FILE__) . '/src/Payzen/Client.php');
